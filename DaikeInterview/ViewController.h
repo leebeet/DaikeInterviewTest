77ecdc89678b0eeb9b4cb7843d6196cd2e752f6c
leebeet <330565133@qq.com>
2016-07-05 14:35:51 +0800

    Initial Commit

---
+++ DaikeInterview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaikeInterview
+//
+//  Created by 李伯通 on 16/7/5.
+//  Copyright © 2016年 李伯通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DaikeInterview/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DaikeInterview
+//
+//  Created by 李伯通 on 16/7/5.
+//  Copyright © 2016年 李伯通. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
