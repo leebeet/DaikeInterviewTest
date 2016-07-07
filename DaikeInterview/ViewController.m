231155913533a5e57bb05cd3b0d91cca8f1083ba
leebeet <330565133@qq.com>
2016-07-07 17:59:00 +0800

    1. add 3.game of life solution 2. improve note 3. add detail notes for 3.game of life (most difficult part of this test i think)

--- DaikeInterview/ViewController.m
+++ DaikeInterview/ViewController.m
@@ -17,23 +17,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //1. Product Array result
+    //1. Product Array testing
     NSArray *arr0 = @[@1, @2, @3, @4];
     NSLog(@"Product Array: %@, result: %@", arr0, [self productArrayWithSource:arr0]);
     
-    //2. Number First result
+    //2. Number First testing
     NSArray *arr1 = @[@0, @1, @0, @3, @12];
     NSLog(@"Number First: %@, result: %@", arr1, [self NumberFirstWithSource:arr1]);
     
-    //4. Simple root square
+    //3. Game of life testing
+    NSMutableArray *arr = [self randomBoardForGameOfLife];
+    NSLog(@"Game of life original random board: %@", arr);
+    [self gameOfLifeWithBoard:arr];
+    NSLog(@"Game of life board after next generation: %@", arr);
+    
+    //4. Simple root square testing
     int calValue = 4;
     NSLog(@"Simple root square: %d, result: %d", calValue, [self sqrtCalculate:calValue]);
     
-    //5. Power
+    //5. Power testing
     double x = 2;
     int    y = 8;
     NSLog(@"Power value with x: %lf, with y: %d, result: %lf", x, y, [self powerWithValue:x factorial:y]);
 }
+
+#pragma mark - Test Answers
 
 //1. Product Array implementation
 - (NSMutableArray *)productArrayWithSource:(NSArray *)sources {
@@ -73,6 +81,81 @@
     return numberFirsts;
 }
 
+//3. Game Of Life implementation
+- (void)gameOfLifeWithBoard:(NSMutableArray *)board
+{
+    //cell value explaination
+    //cell value 0: dead to dead
+    //cell value 1: live to live
+    //cell value 2: live to dead
+    //cell value 3: dead to live
+    
+    NSUInteger lines = board.count;
+    NSUInteger columes = [[board objectAtIndex:0] count];
+    
+    //assign board cells value
+    for(int i = 0; i < lines; i++) {
+        for(int j = 0; j < columes; j++) {
+            int liveCells = [self detectLiveCellsWithBoard:board lines:(int)lines columes:(int)columes positionX:i positionY:j];
+            
+            //live cell with fewer than two live neighbors dies
+            //live cell with more than three live neighbors dies
+            if([board[i][j] intValue] == 1 && (liveCells < 2 || liveCells > 3) ) {
+                board[i][j] = @2;
+            }
+            //Any dead cell with exactly three live neighbors becomes a live cell
+            if([board[i][j] intValue] == 0  && liveCells == 3) {
+                board[i][j] = @3;
+            }
+        }
+    }
+    
+    //update board cells state in-place, at the same time
+    //transfer board cells value to cell state --- live (1) or dead (0)
+    for (int i = 0; i < lines; ++i) {
+        for (int j = 0; j < columes; ++j) {
+            board[i][j] = [NSNumber numberWithInt:[board[i][j] intValue] % 2];
+        }
+    }
+}
+
+//detect how many live cells around target cell on position (x, y)
+- (int)detectLiveCellsWithBoard:(NSMutableArray *)board lines:(int)lines columes:(int)columes positionX:(int)x positionY:(int)y {
+    
+    int liveCells = 0;
+    
+    //solve the board size border problem before cycling / detect live cell
+    int initIndexX  = (((x - 1) >= 0)      ? (x - 1) : 0);
+    int linesCount  = (((x + 1) < lines)   ? (x + 1) : x);
+    int initIndexY  = (((y - 1) >= 0)      ? (y - 1) : 0);
+    int columesCount= (((y + 1) < columes) ? (y + 1) : y);
+    
+    // start to cycling to canculate how many live cells around position(x, y) cell
+    // then return the live cell count
+    for (int i = initIndexX; i <= linesCount; i++) {
+        for (int j = initIndexY; j <= columesCount; j++) {
+            
+            if (!((i == x) && (j == y))) {
+                
+                int neighborOriginState = [board[i][j] intValue];
+                
+                if ([board[i][j] intValue] == 2) {
+                    neighborOriginState = 1;
+                }
+                
+                else if ([board[i][j] intValue] == 3) {
+                    neighborOriginState = 0;
+                }
+                
+                if (neighborOriginState == 1) {
+                    liveCells++;
+                }
+            }
+        }
+    }
+    return liveCells;
+}
+
 //4. Simple root square implementation
 - (int)sqrtCalculate:(int) x{
     
@@ -89,14 +172,32 @@
 - (double)powerWithValue:(double)x factorial:(int)n
 {
     if(n==0)
-         return 1.0;
+        return 1.0;
     
     else if(n<0)
-         return 1.0/[self powerWithValue:x factorial:-n];
+        return 1.0/[self powerWithValue:x factorial:-n];
     
     else return x * [self powerWithValue:x factorial:n - 1];
 }
 
+#pragma mark - Testing Methods
+
+- (NSMutableArray *)randomBoardForGameOfLife {
+    
+    //create a random board under 6 * 6 limit (u can set any board size by changing below divided number)
+    int lines  = arc4random() % 6 + 1;
+    int columes = arc4random() % 6 + 1;
+    
+    NSMutableArray *arr = [NSMutableArray new];
+    for (int i = 0; i < lines; i ++) {
+        NSMutableArray *arrElement = [NSMutableArray new];
+        for (int j = 0; j < columes; j ++) {
+            [arrElement addObject:[NSNumber numberWithInt:arc4random() % 2]];
+        }
+        [arr addObject:arrElement];
+    }
+    return arr;
+}
 @end
 
 
